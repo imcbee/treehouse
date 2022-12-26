6c5e242a8cf280a9cada16bd715459a0d045200b
Ian McBee <imcbee@terpmail.umd.edu>
2022-12-26 14:47:36 -0600

    bug on getting whatisyourname working

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,44 @@
 
 use ::std::io::stdin;
 
+const VISITOR_LIST: [&str; 3] = ["bert", "steve", "ian"];
+
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
+    let mut allow_them_in = false;
 
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
 
-    if your_name == "bert" {
-        println!("Welcome!");
+    for visitor in &VISITOR_LIST {
+        if visitor == &your_name {
+            allow_them_in = true;
+            println!("it is working");
+        }
+        println!("{}", visitor);
+    }
+    println!("{}", allow_them_in);
+
+    if allow_them_in {
+        println!("Welcome! to the treehouse, {}", your_name);
     }else{
         println!("Sorry, you are not on the list");
     }
+
     your_name.trim().to_lowercase()
 }
 
 fn main() {
+
     println!("Hello, what's your name?");
     let name = what_is_your_name();
+
+    // for i in 0..10 {
+    //     println!("{}", i)
+    // }
+
     println!("Hello, {:?}", name)
     
 }
